c6df1aa03f64c9520215203b034b0947b5845c21
Tumelo20 <51445929+Tumelo20@users.noreply.github.com>
2019-10-24 16:43:59 +0200

    first commit

---
+++ LoginAPI/Controllers/CredentialsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataAccess;
+
+namespace LoginAPI.Controllers
+{
+    public class CredentialsController : ApiController
+    {
+        public IEnumerable<Credential> Get()
+        {
+            using(CredentialsEntities entities = new CredentialsEntities())
+            {
+                return entities.Credentials.ToList();
+            }
+        }
+
+        public Credential Get(string username)
+        {
+            using(CredentialsEntities entities = new CredentialsEntities())
+            {
+                return entities.Credentials.FirstOrDefault(e => e.Username == username);
+            }
+        }
+
+    }
+}
---
+++ LoginAPI/Controllers/DetailsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataAccess;
+
+namespace LoginAPI.Controllers
+{
+    public class DetailsController : ApiController
+    {
+        public IEnumerable<Detail> Get()
+        {
+            using (CredentialsEntities entities = new CredentialsEntities())
+            {
+                return entities.Details.ToList();
+            }
+        }
+
+        public Detail Get(int id)
+        {
+            using (CredentialsEntities entities = new CredentialsEntities())
+            {
+                return entities.Details.FirstOrDefault(e => e.IdNo == id);
+            }
+        }
+    }
+}
